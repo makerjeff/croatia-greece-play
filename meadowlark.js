7d27a63743bf03a89e22747954ff20577a68f113
Jeff Wu <makerjeffwu@gmail.com>
2016-07-22 16:48:43 -0700

    fixed the logo issue, it was a jpg instead of a png. also created working 'fortune' section, now moving on to making it a package

--- meadowlark.js
+++ meadowlark.js
@@ -6,24 +6,39 @@
 var express = require('express');
 var app = express();
 
+//fortunes
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
+
 //set up Handlebars as view engine
 // var handlebars = require('express-handlebars').create({defaultLayout:'main});
 var handlebarsModule = require('express-handlebars');
 var handlebars = handlebarsModule.create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
-
 app.set('view engine', 'handlebars');
 
 //an app setter
 app.set('port', process.env.PORT || 3000);
+
+//debug log middleware (JWX)
+app.use(function(req,res,next){
+    console.log(req.url);
+    next();
+});
 
 // ---- routes ----
 app.get('/', function(req, res){
     res.render('home');
 });
 app.get('/about', function(req, res){
-    res.render('about');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
 });
 
 // ---- middlware -----
